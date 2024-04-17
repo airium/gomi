72ab53450dd7582b46a25b6f5ee9321fbf88bf78
airium <airium@outlook.com>
2024-04-17 14:29:49 +0100

    Update translations

--- gomi/__i18n/en_GB.py
+++ gomi/__i18n/en_GB.py
@@ -8,3 +8,15 @@ PROMPT_PWD_0 = "Please input the password (or ctrl+c to give up): "
 SSD_CHECKER_KEY_ERROR_1 = 'KeyError when checking if it\'s SSD for "{}".'
 SSD_CHECKER_UNK_ERROR_2 = 'Unknown error {} when checking if it\'s SSD for "{}".'
 UNHANDLED_ARC_FMT_1 = 'Unhandled archive format "{}". Please report to request a feature update.'
+
+THIS_MODULE_REQUIRES_PYTHON_1 = "This module requires Python {}."
+
+STARTED_AT_1 = "Started at {time}"
+STARTED_AT_2 = "{name} started at {time}"
+FINISHED_0 = "Finished."
+FINISHED_3 = "{name} finished at {time}, elapsed {elapsed:.3f}s"
+ERRORED_AT_1 = "Errored at {time} WITH EXCEPTION."
+FINISHED_AT_1 = "Finished at {time} without uncaught exception."
+HIT_EXCEP_0 = "↑↑↑ Hit uncaught exception! Please check/report ↑↑↑"
+EXITING_0 = "Exiting ...          "
+EXITING_IN_1 = "Exiting in {time}s..."
--- gomi/wrappers.py
+++ gomi/wrappers.py
@@ -5,25 +5,17 @@ import traceback
 from typing import Callable
 
 from .strings import YYMMDD_HHMMSS
+from .__i18n import *
 
-_START_1 = "Started at {time}"
-_START_2 = "{name} started at {time}"
-_FINISH_0 = "Finished."
-_FINISH_3 = "{name} finished at {time}, elapsed {elapsed:.3f}s"
-_FINISH_WE_1 = "Interrupted at {time} WITH EXCEPTION."
-_FINISH_WO_1 = "Finished at {time} without uncaught exception."
-_HIT_EXP_0 = "↑↑↑ Hit uncaught exception! Please check/report ↑↑↑"
-_EXITING_0 = "Exiting ...          "
-_EXITING_1 = "Exiting in {time}s..."
 
 __EXIT_WAIT = 5
 
 
 def _exit_prompt(wait: int = 5):
     for i in range(max(wait, 1), 0, -1):
-        print(_EXITING_1.format(time=i), end="\r")
+        print(EXITING_IN_1.format(time=i), end="\r")
         time.sleep(1)
-    print(_EXITING_0)
+    print(EXITING_0)
 
 
 def tryCall(_callable: Callable, *args, **kwargs):
@@ -39,12 +31,12 @@ def tryMain(_callable: Callable, *args, _exit_wait: int = __EXIT_WAIT, **kwargs)
     except SystemExit as e:
         if e.code != 0:
             traceback.print_exc()
-            input(_HIT_EXP_0)
+            input(HIT_EXCEP_0)
     except:
         traceback.print_exc()
-        input(_HIT_EXP_0)
+        input(HIT_EXCEP_0)
     else:
-        print(_FINISH_0)
+        print(FINISHED_0)
         _exit_prompt(wait=_exit_wait)
         exit(0)
     finally:
@@ -53,17 +45,17 @@ def tryMain(_callable: Callable, *args, _exit_wait: int = __EXIT_WAIT, **kwargs)
 
 def tryTimedMain(_callable: Callable, *args, _exit_wait: int = __EXIT_WAIT, **kwargs):
     try:
-        print(_START_1.format(time=YYMMDD_HHMMSS))
+        print(STARTED_AT_1.format(time=YYMMDD_HHMMSS))
         _callable(*args, **kwargs)
     except SystemExit as e:
         if e.code != 0:
             traceback.print_exc()
-            print(_FINISH_WE_1.format(time=YYMMDD_HHMMSS))
+            print(ERRORED_AT_1.format(time=YYMMDD_HHMMSS))
     except:
         traceback.print_exc()
-        print(_FINISH_WE_1.format(time=YYMMDD_HHMMSS))
+        print(ERRORED_AT_1.format(time=YYMMDD_HHMMSS))
     else:
-        print(_FINISH_WO_1.format(time=YYMMDD_HHMMSS))
+        print(FINISHED_AT_1.format(time=YYMMDD_HHMMSS))
         _exit_prompt(wait=_exit_wait)
         exit(0)
     finally:
@@ -72,8 +64,8 @@ def tryTimedMain(_callable: Callable, *args, _exit_wait: int = __EXIT_WAIT, **kw
 
 def callTimed(_callable: Callable, *args, **kwargs):
     start = time.perf_counter()
-    print(_START_2.format(name=_callable.__name__, time=YYMMDD_HHMMSS))
+    print(STARTED_AT_2.format(name=_callable.__name__, time=YYMMDD_HHMMSS))
     ret = _callable(*args, **kwargs)
     end = time.perf_counter()
-    print(_FINISH_3.format(name=_callable.__name__, time=YYMMDD_HHMMSS, elapsed=end - start))
+    print(FINISHED_3.format(name=_callable.__name__, time=YYMMDD_HHMMSS, elapsed=end - start))
     return ret
