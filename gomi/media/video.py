c285d4cfcb957391b955792750488236792f7841
airium <airium@outlook.com>
2024-04-23 15:57:13 +0100

    Add media/video module

---
+++ gomi/media/video.py
@@ -0,0 +1,12 @@
+__all__ = ["gvfc", "get_video_framecount"]
+
+from .mediainfo import getMediaInfo
+
+
+def get_video_framecount(path) -> int:
+    for track in getMediaInfo(path).video_tracks:
+        return int(getattr(track, "frame_count", 0))
+    return 0
+
+
+getVID_FC = get_video_framecount
