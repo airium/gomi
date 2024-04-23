f9809e41f0c59676d422733c2e718b894a3725f9
airium <airium@outlook.com>
2024-04-23 15:57:01 +0100

    Add typing module

---
+++ gomi/typing.py
@@ -0,0 +1,9 @@
+__all__ = ["toNamedTuple"]
+
+from collections import namedtuple
+from typing import Optional
+
+
+def toNamedTuple(d: dict, name: Optional[str] = None):
+    assert isinstance(d, dict)
+    return namedtuple(name or "AnonymousNamedTuple", d.keys())(**d)
