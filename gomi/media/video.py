036c15f76c0ee72f2300c309238de618d03bbe85
airium <airium@outlook.com>
2024-06-13 12:07:13 +0100

    Update Video module

--- gomi/media/video.py
+++ gomi/media/video.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ["gvfc", "get_video_framecount"]
+__all__ = ["getVID_FC", "get_video_framecount", "get_video_frame_rate", "get_video_time_length"]
 
 from .mediainfo import getMediaInfo
 
@@ -9,6 +9,18 @@ def get_video_framecount(path) -> int:
     for track in getMediaInfo(path).video_tracks:
         return int(getattr(track, "frame_count", 0))
     return 0
+
+
+def get_video_frame_rate(path) -> float:
+    for track in getMediaInfo(path).video_tracks:
+        return float(getattr(track, "frame_rate", 0))
+    return 0.0
+
+
+def get_video_time_length(path) -> float:
+    for track in getMediaInfo(path).video_tracks:
+        return float(getattr(track, "duration", 0))
+    return 0.0
 
 
 getVID_FC = get_video_framecount
